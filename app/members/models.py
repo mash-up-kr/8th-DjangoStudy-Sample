f7198602c8729904cd00fbf48bc69790171847bb
이한영 <dev@lhy.kr>
2019-11-30 13:31:09 +0900

    User모델 필드 추가, requirements수정
    - User모델에 nickname, img_profile필드 추가
    - Pillow를 requirements에 추가

--- app/members/models.py
+++ app/members/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     """
     Custom UserModel정의
     """
-    pass
+    nickname = models.CharField('닉네임', max_length=50, blank=True)
+    img_profile = models.ImageField('프로필 이미지', blank=True)
